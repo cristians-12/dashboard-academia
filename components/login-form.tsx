b76afc537a5a3067608e250c7b6e385d19f770d9
Cristian <97061228+cristians-12@users.noreply.github.com>
2025-06-28 17:02:20 -0500

    feat: login identification number

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -36,12 +36,17 @@ export function LoginForm({
     setError(null);
 
     try {
-      const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
-      if (error) throw error;
-      // Update this route to redirect to an authenticated route. The user already has an active session.
+      // const { error } = await supabase.auth.signInWithPassword({
+      //   email,
+      //   password,
+      // });
+      const { data, error } = await supabase
+        .from("academia")
+        .select("*")
+        .eq("identification_number", identificacion)
+        .single(); // single lanza error si hay 0 o más de 1 resultado
+
+      // if (error) throw error;
       router.push("/protected");
     } catch (error: unknown) {
       setError(error instanceof Error ? error.message : "An error occurred");
